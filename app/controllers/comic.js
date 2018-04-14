b9df2c4248579274319553e641fc11ed7cd51326
malyon <michael.lyon@hotmail.com>
2018-04-14 10:26:34 -0500

    Fixed broken comic src page.
    Src page of comic was broken.  Now you must go to the url
    with the date or you can't see the comic appear.  This may mean
    that no comic will show in the early hours of a day

--- app/controllers/comic.js
+++ app/controllers/comic.js
@@ -4,6 +4,7 @@ var logger = require('winston');
 var config = require('../../config/config');
 var cheerio = require('cheerio');
 var request = require('request');
+var moment = require('moment-timezone');
 
 module.exports = function (app) {
   app.use('/comic', router);
@@ -13,8 +14,9 @@ module.exports = function (app) {
 // TODO: Get the auth token from the jwt and make the calls to get the events,
 //  pass the events into the home route.
 router.get('/', function (req, res, next) {
+  var date = moment().tz('America/Chicago').format('YYYY/MM/DD');;
   logger.log('debug', 'Parsing html string for comic src');
-  request('http://www.gocomics.com/peanuts', function(error, response, body) {
+  request('http://www.gocomics.com/peanuts/' + date, function(error, response, body) {
     if (error !== null)
     {
       res.status(500).json({error: 'There was a problem with the get request to gocomics.com'});
